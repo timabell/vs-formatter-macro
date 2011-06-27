8cda08cfed913b4229013020b7f86646bc739dac
James Nylen <jnylen@gmail.com>
2011-06-27 11:06:59 -0400

    Change tabs to spaces

--- Formatting.vb
+++ Formatting.vb
@@ -33,91 +33,91 @@ Imports System.Text
 
 Public Module Formatting
 
-	Dim allowed As List(Of String) = New List(Of String)
-	Dim processed As Integer = 0
-	Dim ignored As Integer = 0
-	Dim errors As StringBuilder = New StringBuilder()
-	Dim skippedExtensions As List(Of String) = New List(Of String)
+    Dim allowed As List(Of String) = New List(Of String)
+    Dim processed As Integer = 0
+    Dim ignored As Integer = 0
+    Dim errors As StringBuilder = New StringBuilder()
+    Dim skippedExtensions As List(Of String) = New List(Of String)
 
-	Public Sub FormatProject()
-		allowed.Add(".master")
-		allowed.Add(".aspx")
-		allowed.Add(".ascx")
-		allowed.Add(".asmx")
-		allowed.Add(".cs")
-		allowed.Add(".vb")
-		allowed.Add(".config")
-		allowed.Add(".css")
-		allowed.Add(".htm")
-		allowed.Add(".html")
-		allowed.Add(".js")
-		Try
-			recurseSolution(AddressOf processItem)
-		Catch ex As Exception
-			Debug.Print("error in main loop: " + ex.ToString())
-		End Try
-		Debug.Print("processed items: " + processed.ToString())
-		Debug.Print("ignored items: " + ignored.ToString())
-		Debug.Print("ignored extensions: " + String.Join(" ", skippedExtensions.ToArray()))
-		Debug.Print(errors.ToString())
-	End Sub
+    Public Sub FormatProject()
+        allowed.Add(".master")
+        allowed.Add(".aspx")
+        allowed.Add(".ascx")
+        allowed.Add(".asmx")
+        allowed.Add(".cs")
+        allowed.Add(".vb")
+        allowed.Add(".config")
+        allowed.Add(".css")
+        allowed.Add(".htm")
+        allowed.Add(".html")
+        allowed.Add(".js")
+        Try
+            recurseSolution(AddressOf processItem)
+        Catch ex As Exception
+            Debug.Print("error in main loop: " + ex.ToString())
+        End Try
+        Debug.Print("processed items: " + processed.ToString())
+        Debug.Print("ignored items: " + ignored.ToString())
+        Debug.Print("ignored extensions: " + String.Join(" ", skippedExtensions.ToArray()))
+        Debug.Print(errors.ToString())
+    End Sub
 
-	Private Sub processItem(ByVal Item As ProjectItem)
-		If Not Item.Name.Contains(".") Then
-			'Debug.Print("no file extension. ignoring.")
-			ignored += 1
-			Return
-		End If
-		Dim ext As String
-		ext = Item.Name.Substring(Item.Name.LastIndexOf("."))	'get file extension
-		If allowed.Contains(ext) Then
-			formatItem(Item)
-			processed += 1
-		Else
-			'Debug.Print("ignoring file with extension: " + ext)
-			If Not skippedExtensions.Contains(ext) Then
-				skippedExtensions.Add(ext)
-			End If
-			ignored += 1
-		End If
-	End Sub
+    Private Sub processItem(ByVal Item As ProjectItem)
+        If Not Item.Name.Contains(".") Then
+            'Debug.Print("no file extension. ignoring.")
+            ignored += 1
+            Return
+        End If
+        Dim ext As String
+        ext = Item.Name.Substring(Item.Name.LastIndexOf("."))   'get file extension
+        If allowed.Contains(ext) Then
+            formatItem(Item)
+            processed += 1
+        Else
+            'Debug.Print("ignoring file with extension: " + ext)
+            If Not skippedExtensions.Contains(ext) Then
+                skippedExtensions.Add(ext)
+            End If
+            ignored += 1
+        End If
+    End Sub
 
-	Private Sub formatItem(ByVal Item As ProjectItem)
-		Debug.Print("processing file " + Item.Name)
-		Try
-			Dim window As EnvDTE.Window
-			window = Item.Open()
-			window.Activate()
-			DTE.ExecuteCommand("Edit.FormatDocument", "")
-			window.Document.Save()
-			window.Close()
-		Catch ex As Exception
-			Debug.Print("error processing file." + ex.ToString())
-			errors.Append("error processing file " + Item.Name + "  " + ex.ToString())
-		End Try
-	End Sub
+    Private Sub formatItem(ByVal Item As ProjectItem)
+        Debug.Print("processing file " + Item.Name)
+        Try
+            Dim window As EnvDTE.Window
+            window = Item.Open()
+            window.Activate()
+            DTE.ExecuteCommand("Edit.FormatDocument", "")
+            window.Document.Save()
+            window.Close()
+        Catch ex As Exception
+            Debug.Print("error processing file." + ex.ToString())
+            errors.Append("error processing file " + Item.Name + "  " + ex.ToString())
+        End Try
+    End Sub
 
-	Private Delegate Sub task(ByVal Item As ProjectItem)
+    Private Delegate Sub task(ByVal Item As ProjectItem)
 
-	Private Sub recurseSolution(ByVal taskRoutine As task)
-		For Each Proj As Project In DTE.Solution.Projects
-			Debug.Print("project " + Proj.Name)
-			For Each Item As ProjectItem In Proj.ProjectItems
-				recurseItems(Item, 0, taskRoutine)
-			Next
-		Next
-	End Sub
+    Private Sub recurseSolution(ByVal taskRoutine As task)
+        For Each Proj As Project In DTE.Solution.Projects
+            Debug.Print("project " + Proj.Name)
+            For Each Item As ProjectItem In Proj.ProjectItems
+                recurseItems(Item, 0, taskRoutine)
+            Next
+        Next
+    End Sub
 
-	Private Sub recurseItems(ByVal Item As ProjectItem, ByVal depth As Integer, ByVal taskRoutine As task)
-		Dim indent As String = New String("-", depth)
-		Debug.Print(indent + " " + Item.Name)
-		taskRoutine(Item)
-		If Not Item.ProjectItems Is Nothing Then
-			For Each Child As ProjectItem In Item.ProjectItems
-				taskRoutine(Child)
-				recurseItems(Child, depth + 1, taskRoutine)
-			Next
-		End If
-	End Sub
+    Private Sub recurseItems(ByVal Item As ProjectItem, ByVal depth As Integer, ByVal taskRoutine As task)
+        Dim indent As String = New String("-", depth)
+        Debug.Print(indent + " " + Item.Name)
+        taskRoutine(Item)
+        If Not Item.ProjectItems Is Nothing Then
+            For Each Child As ProjectItem In Item.ProjectItems
+                taskRoutine(Child)
+                recurseItems(Child, depth + 1, taskRoutine)
+            Next
+        End If
+    End Sub
 
 End Module
